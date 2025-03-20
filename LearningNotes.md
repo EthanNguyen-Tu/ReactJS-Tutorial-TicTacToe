577127bb434f8f172e6bb94b79e1b8033f0b25e2
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-03-20 00:04:49 -0400

    style(format): Formatted code with Prettier

--- LearningNotes.md
+++ LearningNotes.md
@@ -1,64 +1,71 @@
 # [Tutorial: Tic-Tac-Toe](https://react.dev/learn/tutorial-tic-tac-toe)
 
-
 ## Terminology
-- component = a piece of reusable code that represents a part of a user interface
-  - used to render, manage, and update UI elements of an application
-- `JSX element` = a combination of JavaScript code and HTML tags that describe what should be displayed
-- `</___>` = closes a JSX element specified by ___ to indicate that any following content should not be placed inside the button
-- `styles.css` = file that defines the styles of the React App
-  - `*` and `body` = define the style of large parts of the app
-  - `.___` = selector that defines the style of any component where the className property is set to ___
-- `index.js` = the bridge between components and the web browser
-- `state` = used by components to remember things
-- `useState` = special React function that can be called by a componbent to let it remember things
-- `props` = allows a parent component to pass a state back down to its children that keeps them in sync with each other and their parent
 
+-   component = a piece of reusable code that represents a part of a user interface
+    -   used to render, manage, and update UI elements of an application
+-   `JSX element` = a combination of JavaScript code and HTML tags that describe what should be displayed
+-   `</___>` = closes a JSX element specified by \_\_\_ to indicate that any following content should not be placed inside the button
+-   `styles.css` = file that defines the styles of the React App
+    -   `*` and `body` = define the style of large parts of the app
+    -   `.___` = selector that defines the style of any component where the className property is set to \_\_\_
+-   `index.js` = the bridge between components and the web browser
+-   `state` = used by components to remember things
+-   `useState` = special React function that can be called by a componbent to let it remember things
+-   `props` = allows a parent component to pass a state back down to its children that keeps them in sync with each other and their parent
 
 ## React Requirements
+
 1. React components need to return a single JSX element and not multiple adjacent JSX elements
 
-
 ## Code Snippets
+
 ```
 export default function Square() {
   return <button className="square">X</button>;
 }
 ```
-- `{___}` = curly braces; used to escape from JSX into JavaScript
-- `arr.slice()` = method that returns the selected elements in an array as a new array without changing the original array; selects from a given start up to a not inclusive given end
-- `<button>` = JSX element
-- `className='___'` = button property/prop that tells CSS how to style the button
-- `default` = keyword taht tells other files in the code that the attributed function is the main funciton of the file
-- `export` = JavaScript keyword that makes a function accessible outside of a file
-- `...history` = spread syntax that enumerates all the items in array history
-- `return` = JavaScript keyword that means whatever comes after is returned as a value to the function's caller
 
+-   `{___}` = curly braces; used to escape from JSX into JavaScript
+-   `arr.slice()` = method that returns the selected elements in an array as a new array without changing the original array; selects from a given start up to a not inclusive given end
+-   `<button>` = JSX element
+-   `className='___'` = button property/prop that tells CSS how to style the button
+-   `default` = keyword taht tells other files in the code that the attributed function is the main funciton of the file
+-   `export` = JavaScript keyword that makes a function accessible outside of a file
+-   `...history` = spread syntax that enumerates all the items in array history
+-   `return` = JavaScript keyword that means whatever comes after is returned as a value to the function's caller
 
 ## Run Project
+
 Locally:
+
 ```
 npm start
 ```
+
 Access [HERE](http://localhost:3000/)
+
 > In Chrome, access console with "option + command + j"
 
 ## Tools
+
 [React Developer Tools (Chrome)](https://chromewebstore.google.com/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi?hl=en)
 
-
 ## HTML
+
 ### Elements
--  `<div>` - by default a block element (takes all available width, and comes with line breaks before and after); used as a container for other HTML elements
+
+-   `<div>` - by default a block element (takes all available width, and comes with line breaks before and after); used as a container for other HTML elements
 
 ### [Basic Formatting](https://www.w3schools.com/html/html_formatting.asp)
-- `<b>` - Bold text
-- `<del>` - Deleted text
-- `<em>` - Emphasized text
-- `<i>` - Italic text
-- `<ins>` - Inserted text
-- `<mark>` - Marked text
-- `<small>` - Smaller text
-- `<strong>` - Important text
-- `<sub>` - Subscript text
-- `<sup>` - Superscript text
+
+-   `<b>` - Bold text
+-   `<del>` - Deleted text
+-   `<em>` - Emphasized text
+-   `<i>` - Italic text
+-   `<ins>` - Inserted text
+-   `<mark>` - Marked text
+-   `<small>` - Smaller text
+-   `<strong>` - Important text
+-   `<sub>` - Subscript text
+-   `<sup>` - Superscript text
--- src/App.js
+++ src/App.js
@@ -1,149 +1,155 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
-  return (
-    <button 
-      className="square"
-      onClick={onSquareClick}
-    >
-      {value}
-    </button>
-  );
+    return (
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
+    );
 }
 
+export function Board({ xIsNext, squares, onPlay, sideSize }) {
+    const winner = checkWinner(squares, sideSize);
 
-export function Board({xIsNext, squares, onPlay, sideSize}) {
+    function handleClick(i) {
+        if (squares[i] || winner) {
+            // checks to make sure that the square tile does not already have a mark or if a winner has been decided
+            return;
+        }
 
-  const winner = checkWinner(squares, sideSize);
-
-  function handleClick(i) {
-    if (squares[i] || winner) { // checks to make sure that the square tile does not already have a mark or if a winner has been decided
-      return;
+        const nextSquares = squares.slice();
+        nextSquares[i] = xIsNext ? "X" : "O";
+        onPlay(nextSquares);
     }
 
-    const nextSquares = squares.slice();
-    nextSquares[i] = (xIsNext) ? "X" : "O";
-    onPlay(nextSquares);
-  }
-
-  let status;
-  if (winner) {
-    status = "The winner is " + winner + "!";
-  } else if (squares.every(val => val != null)) {
-    status = "You have achieved a TIE!"
-  } else {
-    status = "Next Player: " + (xIsNext ? "X" : "O")
-  }
-
-  function generateSquareBoard(sideSize) {
-    const row = []
-    for (let i = 0; i < sideSize; i++) {
-      const col = []
-      for (let j = sideSize * i; j < (sideSize * (i + 1)); j++) {
-        col.push(<Square key={j} value={squares[j]} onSquareClick={() => handleClick(j)} />);
-      }
-      row.push(<div key={i} className="board-row">{col}</div>);
+    let status;
+    if (winner) {
+        status = "The winner is " + winner + "!";
+    } else if (squares.every((val) => val != null)) {
+        status = "You have achieved a TIE!";
+    } else {
+        status = "Next Player: " + (xIsNext ? "X" : "O");
     }
-    return row;
-  }
 
-  return (
-    <>
-      <div className='status'>{status}</div>
-      {
-      generateSquareBoard(sideSize)
-      }
-    </>
-  );
+    function generateSquareBoard(sideSize) {
+        const row = [];
+        for (let i = 0; i < sideSize; i++) {
+            const col = [];
+            for (let j = sideSize * i; j < sideSize * (i + 1); j++) {
+                col.push(
+                    <Square
+                        key={j}
+                        value={squares[j]}
+                        onSquareClick={() => handleClick(j)}
+                    />
+                );
+            }
+            row.push(
+                <div key={i} className="board-row">
+                    {col}
+                </div>
+            );
+        }
+        return row;
+    }
+
+    return (
+        <>
+            <div className="status">{status}</div>
+            {generateSquareBoard(sideSize)}
+        </>
+    );
 }
-
 
 export default function Game() {
-  const sideSize = 5;
-  const [history, setHistory] = useState([Array(sideSize ** 2).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
+    const sideSize = 5;
+    const [history, setHistory] = useState([Array(sideSize ** 2).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
 
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
 
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+    }
 
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
+    const moves = history.map((squares, move) => {
+        let description = move > 0 ? "Go to move #" + move : "Go to game start";
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
 
-  const moves = history.map((squares, move) => {
-    let description = (move > 0) ? "Go to move #" + move : "Go to game start";
     return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    xIsNext={xIsNext}
+                    squares={currentSquares}
+                    onPlay={handlePlay}
+                    sideSize={sideSize}
+                />
+            </div>
+            <div className="game-info">
+                <h3>Game History:</h3>
+                <ol>{moves}</ol>
+            </div>
+        </div>
     );
-  });
-
-  return (
-    <div className='game'>
-      <div className='game-board'>
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} sideSize={sideSize}/>
-      </div>
-      <div className='game-info'>
-        <h3>Game History:</h3>
-        <ol>{moves}</ol>
-      </div>
-    </div>
-  );
 }
-
 
 //===[ NOT COMPONENTS ]===
 
 function checkWinner(squares, sideSize) {
-  /*
+    /*
   Iteratively checks all valid ways to win in Tic-Tac-Toe
   */
-  let arr;
+    let arr;
 
-  // Check each row
-  const allEqual = arr => arr.every(val => val != null && val === arr[0]);
+    // Check each row
+    const allEqual = (arr) => arr.every((val) => val != null && val === arr[0]);
 
-  for (let i = 0; i < squares.length; i += sideSize) {
-    arr = squares.slice(i, i + sideSize);
+    for (let i = 0; i < squares.length; i += sideSize) {
+        arr = squares.slice(i, i + sideSize);
+        if (allEqual(arr)) {
+            return arr[0];
+        }
+    }
+
+    // Check each column
+    for (let i = 0; i < sideSize; i++) {
+        arr = [];
+        for (let j = i; j < squares.length; j += sideSize) {
+            arr.push(squares[j]);
+        }
+        if (allEqual(arr)) {
+            return arr[0];
+        }
+    }
+
+    // Check topleft-botright diagonal
+    for (let i = 0; i < sideSize; i++) {
+        arr[i] = squares[i * (sideSize + 1)];
+    }
     if (allEqual(arr)) {
-      return arr[0];
+        return arr[0];
     }
-  }
 
-  // Check each column
-  for (let i = 0; i < sideSize; i++) {
-    arr = [];
-    for (let j = i; j < squares.length; j += sideSize) {
-      arr.push(squares[j]);
+    // Check topright-botleft diagonal
+    for (let i = 0; i < sideSize; i++) {
+        arr[i] = squares[(i + 1) * (sideSize - 1)];
     }
     if (allEqual(arr)) {
-      return arr[0];
+        return arr[0];
     }
-  }
 
-  // Check topleft-botright diagonal
-  for (let i = 0; i < sideSize; i ++) {
-    arr[i] = squares[i * (sideSize + 1)]
-  }
-  if (allEqual(arr)) {
-    return arr[0];
-  }
-
-  // Check topright-botleft diagonal
-  for (let i = 0; i < sideSize; i ++) {
-    arr[i] = squares[(i + 1) * (sideSize - 1)]
-  }
-  if (allEqual(arr)) {
-    return arr[0];
-  }
-
-  // Neither player has won
-  return null;
+    // Neither player has won
+    return null;
 }
